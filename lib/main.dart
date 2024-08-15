033e3049a437f6c016787374b450a4e1c9d3f141
Matthew <adetoyesesamson@gmail.com>
2024-08-15 12:27:23 +0100

    'commit'

---
+++ lib/main.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product Listing'),
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+        children: const <Widget>[
+          ProductBox(
+              name: "iPhone",
+              description: "iPhone is the stylist phone ever",
+              price: 1000,
+              image: "ade.jpg"),
+          ProductBox(
+              name: "Pixel",
+              description: "Pixel is the most featureful phone ever",
+              price: 800,
+              image: "adewale.PNG"),
+          ProductBox(
+              name: "Laptop",
+              description: "Laptop is most productive development tool",
+              price: 2000,
+              image: "ade.jpg"),
+          ProductBox(
+              name: "Tablet",
+              description: "Tablet is the most useful device ever for meeting",
+              price: 1500,
+              image: 'adewale.PNG'),
+          ProductBox(
+              name: "Pendrive",
+              description: "Pendrive is useful storage medium",
+              price: 100,
+              image: "ade.jpg"),
+          ProductBox(
+              name: "Floppy Drive",
+              description: "Floppy drive is useful rescue storage medium",
+              price: 20,
+              image: "adewale.PNG"),
+        ],
+      ),
+    );
+  }
+}
+
+class RatingBox extends StatefulWidget {
+  const RatingBox({super.key});
+
+  @override
+  State<RatingBox> createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+  void _setRatingAsOne() {
+    setState(() {
+      _rating = 1;
+    });
+  }
+
+  // ignore: unused_element
+  void _setRatingAsTwo() {
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAsThree() {
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double ssize = 20;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            onPressed: () {
+              _setRatingAsOne();
+            },
+            icon: (_rating >= 1
+                ? Icon(
+                    Icons.star,
+                    size: ssize,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: ssize,
+                  )),
+            color: Colors.red[500],
+            iconSize: ssize,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            onPressed:(){},
+            icon: (_rating >= 2
+                ? Icon(
+                    Icons.star,
+                    size: ssize,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: ssize,
+                  )),
+            color: Colors.red[500],
+            iconSize: ssize,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            onPressed: () {
+              _setRatingAsThree();
+            },
+            icon: (_rating >= 3
+                ? Icon(
+                    Icons.star,
+                    size: ssize,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: ssize,
+                  )),
+            color: Colors.red[500],
+            iconSize: ssize,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ProductBox extends StatelessWidget {
+  const ProductBox(
+      {super.key,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.image});
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      height: 150,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Image.asset(
+            'assets/$image',
+            width: 150,
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    description,
+                  ),
+                  // ignore: prefer_interpolation_to_compose_strings
+                  Text('Price:' + price.toString()),
+                  const RatingBox()
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
